5d7cf0b95384017cf72bf31028db0e295017a9f1
EmiNiemand <Szymon_PL@live.com>
2022-05-17 14:52:47 +0200

    final

--- main.py
+++ main.py
@@ -4,58 +4,58 @@ import draw
 
 
 def main():
-    # fun = None
-    # while fun is None:
-    #     fun = int(input("Choose function [1-7]: \n"
-    #                     "1. Linear: 3x - 5\n"
-    #                     "2. Absolute x: |x|\n"
-    #                     "3. Polynomial: 2x^3 + 2x^2 + 4x - 1\n"
-    #                     "4. Trigonometric: sin(1/x)\n"
-    #                     "5. Composite #1: (2x + 3)^|x|\n"
-    #                     "6. Composite #2: (2x + 3)(2x^3 + 2x^2 + 4x - 1)\n"
-    #                     "7. Composite #3: sin(x)*|x|\n"))
-    #     match fun:
-    #         case 1:
-    #             fun = func.linear
-    #         case 2:
-    #             fun = func.abs_x
-    #         case 3:
-    #             fun = func.polynomial
-    #         case 4:
-    #             fun = func.trigonometric
-    #         case 5:
-    #             fun = func.composite_1
-    #         case 6:
-    #             fun = func.composite_2
-    #         case 7:
-    #             fun = func.composite_3
-    #         case _:
-    #             fun = None
-    #
-    # l_range = float(input("Podaj dolny zakres: "))
-    # u_range = float(input("Podaj górny zakres: "))
-    # if l_range > u_range:
-    #     pom = l_range
-    #     l_range = u_range
-    #     u_range = pom
-    #
-    # degree = int(input("Podaj stopień wielomianu: "))
-    # accuracy = float(input("Podaj dokładność: "))
-    # l_args = app.legendre_calculate_arguments(degree)
-    # factors = app.calculate_factors(fun, degree, l_range, u_range, accuracy, l_args)
-    # draw.draw_functions(l_range, u_range, fun, factors, l_args)
+    fun = None
+    while fun is None:
+        fun = int(input("Choose function [1-7]: \n"
+                        "1. Linear: 3x - 5\n"
+                        "2. Absolute x: |x|\n"
+                        "3. Polynomial: 2x^3 + 2x^2 + 4x - 1\n"
+                        "4. Trigonometric: sin(1/x)\n"
+                        "5. Composite #1: (2x + 3)^|x|\n"
+                        "6. Composite #2: (2x + 3)(2x^3 + 2x^2 + 4x - 1)\n"
+                        "7. Composite #3: sin(x)*|x|\n"))
+        match fun:
+            case 1:
+                fun = func.linear
+            case 2:
+                fun = func.abs_x
+            case 3:
+                fun = func.polynomial
+            case 4:
+                fun = func.trigonometric
+            case 5:
+                fun = func.composite_1
+            case 6:
+                fun = func.composite_2
+            case 7:
+                fun = func.composite_3
+            case _:
+                fun = None
+
+    l_range = float(input("Podaj dolny zakres: "))
+    u_range = float(input("Podaj górny zakres: "))
+    if l_range > u_range:
+        pom = l_range
+        l_range = u_range
+        u_range = pom
+
+    degree = int(input("Podaj stopień wielomianu: "))
+    accuracy = float(input("Podaj dokładność: "))
+    l_args = app.legendre_calculate_arguments(degree)
+    factors = app.calculate_factors(fun, degree, l_range, u_range, accuracy, l_args)
+    draw.draw_functions(l_range, u_range, fun, factors, l_args)
     # =====================Tests====================
     # # Func 1
-    fun1 = func.linear
-    l_range = -3
-    u_range = 5
-    degree = 2
-    accuracy = 0.0001
-    for i in range(4):
-        l_args = app.legendre_calculate_arguments(degree)
-        factors = app.calculate_factors(fun1, degree, l_range, u_range, accuracy, l_args)
-        draw.draw_functions(l_range, u_range, fun1, factors, l_args)
-        degree *= 2
+    # fun1 = func.linear
+    # l_range = -3
+    # u_range = 5
+    # degree = 2
+    # accuracy = 0.0001
+    # for i in range(4):
+    #     l_args = app.legendre_calculate_arguments(degree)
+    #     factors = app.calculate_factors(fun1, degree, l_range, u_range, accuracy, l_args)
+    #     draw.draw_functions(l_range, u_range, fun1, factors, l_args)
+    #     degree *= 2
     # # Func 2
     # fun2 = func.abs_x
     # l_range = -3
